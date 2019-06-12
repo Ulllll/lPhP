42247daaa11263ffcf672b60171186b49dcc9bc1
Ulllll <nooryes1@gmail.com>
2019-06-12 21:27:27 +0300

    3

---
+++ Задание 6/index.php
@@ -0,0 +1,70 @@
+<?php 
+    $_title="minimalistica";
+    $_h1="minimalistica"; 
+    $_year=date('Y');
+    ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+	<meta name="author" content="Luka Cvrk (www.solucija.com)" />
+	<link rel="stylesheet" href="css/main.css" type="text/css" />
+	<title><?php echo $_title; ?></title>
+</head>
+<body>
+
+	<div id="content">
+		<h1><?=$_h1;?></h1>
+		<? //Задание 6
+        $ul="<ul id=\"menu\">";
+        $i=1;
+        while($i<4){
+            if($i==1){
+                $ul.="<li><a href=\"#\">home</a></li>";
+            }
+            if($i==2){
+                $ul.="<li><a href=\"#\">archive</a></li>";
+            }
+            if($i==3){
+                $ul.="<li><a href=\"#\">contact</a></li>";
+            }
+            $i++;
+        }
+        $ul.="</ul>";
+        echo $ul;
+        ?>
+	
+		<div class="post">
+			<div class="details">
+				<h2><a href="#">Nunc commodo euismod massa quis vestibulum</a></h2>
+				<p class="info">posted 3 hours ago in <a href="#">general</a></p>
+			
+			</div>
+			<div class="body">
+				<p>Nunc eget nunc libero. Nunc commodo euismod massa quis vestibulum. Proin mi nibh, dignissim a pellentesque at, ultricies sit amet sapien. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vel lorem eu libero laoreet facilisis. Aenean placerat, ligula quis placerat iaculis, mi magna luctus nibh, adipiscing pretium erat neque vitae augue. Quisque consectetur odio ut sem semper commodo. Maecenas iaculis leo a ligula euismod condimentum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut enim risus, rhoncus sit amet ultricies vel, aliquet ut dolor. Duis iaculis urna vel massa ultricies suscipit. Phasellus diam sapien, fermentum a eleifend non, luctus non augue. Quisque scelerisque purus quis eros sollicitudin gravida. Aliquam erat volutpat. Donec a sem consequat tortor posuere dignissim sit amet at ipsum.</p>
+			</div>
+			<div class="x"></div>
+		</div>
+		
+		<div class="col">
+			<h3><a href="#">Ut enim risus rhoncus</a></h3>
+			<p>Quisque consectetur odio ut sem semper commodo. Maecenas iaculis leo a ligula euismod condimentum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut enim risus, rhoncus sit amet ultricies vel, aliquet ut dolor. Duis iaculis urna vel massa ultricies suscipit. Phasellus diam sapien, fermentum a eleifend non, luctus non augue. Quisque scelerisque purus quis eros sollicitudin gravida. Aliquam erat volutpat. Donec a sem consequat tortor posuere dignissim sit amet at.</p>
+			<p>&not; <a href="#">read more</a></p>
+		</div>
+		<div class="col">
+			<h3><a href="#">Maecenas iaculis leo</a></h3>
+			<p>Quisque consectetur odio ut sem semper commodo. Maecenas iaculis leo a ligula euismod condimentum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut enim risus, rhoncus sit amet ultricies vel, aliquet ut dolor. Duis iaculis urna vel massa ultricies suscipit. Phasellus diam sapien, fermentum a eleifend non, luctus non augue. Quisque scelerisque purus quis eros sollicitudin gravida. Aliquam erat volutpat. Donec a sem consequat tortor posuere dignissim sit amet at.</p>
+			<p>&not; <a href="#">read more</a></p>
+		</div>
+		<div class="col last">
+			<h3><a href="#">Quisque consectetur odio</a></h3>
+			<p>Quisque consectetur odio ut sem semper commodo. Maecenas iaculis leo a ligula euismod condimentum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut enim risus, rhoncus sit amet ultricies vel, aliquet ut dolor. Duis iaculis urna vel massa ultricies suscipit. Phasellus diam sapien, fermentum a eleifend non, luctus non augue. Quisque scelerisque purus quis eros sollicitudin gravida. Aliquam erat volutpat. Donec a sem consequat tortor posuere dignissim sit amet at.</p>
+			<p>&not; <a href="#">read more</a></p>
+		</div>
+		
+		<div id="footer">
+			<p>Copyright &copy; <?=$_year; ?><em>minimalistica</em> &middot; Design: Luka Cvrk, <a href="http://www.solucija.com/" title="Free CSS Templates">Solucija</a></p>
+		</div>	
+	</div>
+</body>
+</html>
